3365e74a32b650ceb07863f3e6c07518e8cdbb56
DiegoServinHdn <diegoservinhdn@gmail.com>
2022-04-09 20:13:38 -0500

    problema moneda resuelto

--- problems/monedas/instrucciones.md
+++ problems/monedas/instrucciones.md
@@ -4,6 +4,6 @@
 
     S={S1,S2,...,Sm}
     Tenemos una cantidad N (valor>=0) y queremos saber cuantas combinaciones de esas monedas nos permiten lograr la cantidad N
-    Supongamos que S={1,2,3} y N=4 tenemos {1,1,1,1}, {1,1,2}, {2,2}, {1,3}
+    Supongamos que S={1,2,3} y N=4 tenemos {1,3}
 
 
--- problems/monedas/main.py
+++ problems/monedas/main.py
@@ -1,15 +1,26 @@
 
-def operacion(monto:int, denominacion:list):
+def operacion(monto:int, denominaciones:list):
+    monedas = {}
 
-    for i in range(len(denominacion)):
-        logica = monto / denominacion[i]
-        if logica == 0:
-            print(f"moneda" + logica)
+    for denominacion in denominaciones:
+        monedas[denominacion] = monto // denominacion
+        monto = monto % denominacion
+
+    monedas["sobrante"] = monto
+
+    return monedas
+
+    # for i in range(len(denominacion)):
+    #     logica = monto / denominacion[i]
+    #     if logica == 0:
+    #         print(f"moneda" + logica)
 
 
 if __name__ == '__main__':
-    monto = 100
-    denominacion = [10,5]
-    operacion(monto, denominacion)
+    monto = 111
+    denominaciones = [10,5,2]
+    monedas =operacion(monto, denominaciones)
+
+    print(monedas)
 
 
